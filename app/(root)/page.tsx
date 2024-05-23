182f73df9afbeaa27c6508f0faec5578188d757d
LO9487 <lojkeng@gmail.com>
2024-05-23 13:06:39 +0800

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import type { AppProps } from 'next/app'
+export default function Home() {
+  return (
+    <div> 
+      <h1 className='head-text text-left'>Home</h1>
+    </div> 
+  )
+}
