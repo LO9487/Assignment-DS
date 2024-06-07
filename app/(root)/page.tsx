4eab3e742dd7c59fc31d9fcb61bc15fe55a58b94
Nas-Mohd <anasamohammad.school@gmail.com>
2024-06-07 12:58:47 +0800

    TypeScript error fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -25,7 +25,7 @@ async function Home({
 
   // Combine posts and remove duplicates
   const postMap = new Map();
-  recommendedPosts.forEach((post) => postMap.set(post._id.toString(), post));
+  recommendedPosts.forEach((post: { _id: { toString: () => any; }; }) => postMap.set(post._id.toString(), post));
   postsResult.posts.forEach((post: { _id: { toString: () => any; }; }) => postMap.set(post._id.toString(), post));
   const combinedPosts = Array.from(postMap.values());
 
